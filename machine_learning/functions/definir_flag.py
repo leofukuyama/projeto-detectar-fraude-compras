b09620157fcac95c4d2014313259bdb610f4e68b
leofukuyama <leofukuyama22@gmail.com>
2025-07-16 11:30:33 -0300

    feat: inserir flags com o modelo

--- machine_learning/functions/definir_flag.py
+++ machine_learning/functions/definir_flag.py
@@ -1,11 +1,16 @@
 from joblib import load
 from sklearn.ensemble import RandomForestClassifier
 from functions import tratar_dados
-
+import pandas as pd
 
 def inserir_flag(df_sem_tratamento):
     modelo_indicativo = load(filename="machine_learning/modelo_indicativo.joblib")
 
-    df = tratar_dados(df_sem_tratamento)
+    df_tratado = tratar_dados(df_sem_tratamento)
 
+    predicao = modelo_indicativo.predict(df_tratado)
+
+    copia_df = df_sem_tratamento.copy()
+    copia_df["flag"] = predicao
     
+    return copia_df

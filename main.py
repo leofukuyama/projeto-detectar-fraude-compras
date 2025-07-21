cc1b4484934cf56173aebea91638ac7b8ff89bed
leofukuyama <leofukuyama22@gmail.com>
2025-07-21 14:34:10 -0300

    feat: Adicionado comparativo de flags

--- main.py
+++ main.py
@@ -61,6 +61,43 @@ def login_page():
         else:
             st.error("Usuário ou senha incorretos")
 
+# Função para retornar os transações com as duas flags
+def retornar_df_duas_flags():
+    try:
+        load_dotenv()
+        conn = mysql.connector.connect(
+            host=os.getenv("DB_HOST"),
+            database=os.getenv("DB_NAME"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASS")
+        )
+        
+        cursor = conn.cursor()
+
+        cursor.execute("SELECT * FROM bronze.transacoes_com_indicativo_raw;")
+
+        column_names = [i[0] for i in cursor.description]
+
+        data = cursor.fetchall()
+
+        data = pd.DataFrame(data, columns=column_names)
+
+        cursor.close()
+        conn.close()
+
+        return data
+
+    except Error as e:
+        st.error(f"Erro ao conectar ao banco de dados: {e}")
+        return False
+    
+# Formatar valores
+def formatar_valores(valor):
+    valor_formatado = f"{valor:,.2f}".replace(",", "X").replace(".", ",").replace("X", ".")
+
+    return valor_formatado
+
+
 # Página principal (após login)
 def main_page():
     with st.sidebar:        
@@ -105,6 +142,21 @@ def main_page():
     relatorio = avaliar_modelo(modelo_indicativo, df_tratado)
     
     st.markdown(f"```\n{relatorio}\n```")
+
+    df_duas_flags = retornar_df_duas_flags()
+
+    total_original = df_duas_flags[df_duas_flags["flag_fraude_confirmada"] == 1]["valor"].sum()
+    total_modelo = df_duas_flags[df_duas_flags["flag_modelo"] == 1]["valor"].sum()
+    diferenca_valores_totais = total_modelo - total_original
+
+    col1, col2, col3, col4, col5 = st.columns(5)
+    with col1:
+        st.metric("Flags Originais:", f"R$ {formatar_valores(total_original)}")
+    with col2:
+        st.metric("Flags Modelo:", f"R$ {formatar_valores(total_modelo)}")
+    with col3:
+        st.metric("Diferença:", f"R$ {formatar_valores(diferenca_valores_totais)}")
+
 
 # Configuração inicial
 if 'logged_in' not in st.session_state:
